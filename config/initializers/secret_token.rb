8342208b9a5ec7e2ff3289f1a4ac43fa10142acf
jakedahn <jake@markupisart.com>
2012-09-07 12:29:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vishfacts::Application.config.secret_token = '2468cb1ea8b3c1cf0b3000c41fe5b6b9c493cf16bdd9885faaa30ca915afa8c98dc49f30daa64db3fb6b670bfb901dfda5d120ebf5f08e0beedb9e879070ebcc'
